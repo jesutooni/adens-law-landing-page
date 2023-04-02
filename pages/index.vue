71f370b434c4e393e3e8049f35e13c10ed5eaa8d
Jesutooni <edeworuyoyou@gmail.com>
2023-04-02 16:30:02 +0100

    project init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="text-5xl font-medium font-rubik">Hayes Studio</div>
+    <AppButton text="View all practice areas" theme="black" size="md" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
