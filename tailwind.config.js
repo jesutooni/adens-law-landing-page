8ea5b903f31cdc88574a04fd3a8cf851a1e593f3
Jesutooni <edeworuyoyou@gmail.com>
2023-04-02 16:30:31 +0100

    set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#121212",
+        white: {
+          primary: "#FEFEFE",
+        },
+        grey: {
+          100: "#C3C3C3",
+          200: "#616161",
+          300: "#424242",
+        },
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+};
