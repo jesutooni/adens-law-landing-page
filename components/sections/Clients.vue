2d25729b7ab53bee88b183498ff04321250bd76d
Jesutooni <edeworuyoyou@gmail.com>
2023-06-29 15:38:52 +0100

    client logos

---
+++ components/sections/Clients.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="py-16 max-w-screen-xl mx-auto px-6">
+    <h2 class="text-center font-extrabold text-black text-3xl">Our Clients</h2>
+    <div class="flex justify-center items-center flex-wrap gap-10 mt-6">
+      <template v-for="logo in logos" class="">
+        <div class="gray">
+          <img class="w-full h-auto" :src="`/logos/${logo}.svg`" :alt="logo" />
+        </div>
+      </template>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      logos: [
+        "dmw",
+        "vdm",
+        "chef-chi",
+        "twenty-six",
+        "medical-mart",
+        "confam",
+        "peruzzi",
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.gray {
+  filter: grayscale(95%);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
     <PracticeAreas />
     <Testimonials />
     <HireUs />
+    <Clients />
   </main>
 </template>
 
@@ -14,8 +15,9 @@ import About from "~/components/sections/About.vue";
 import PracticeAreas from "~/components/sections/PracticeAreas.vue";
 import Testimonials from "~/components/sections/Testimonials.vue";
 import HireUs from "~/components/sections/HireUs.vue";
+import Clients from "~/components/sections/Clients.vue";
 export default {
   name: "IndexPage",
-  components: { Hero, About, PracticeAreas, Testimonials, HireUs },
+  components: { Hero, About, PracticeAreas, Testimonials, HireUs, Clients },
 };
 </script>
